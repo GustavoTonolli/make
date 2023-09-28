a973d6705127ca96a0f851257f580ed92a8c396e
gustavotonolli <gustavo.tonolli@hotmail.com>
2023-09-28 09:44:32 -0300

    angular 16

--- src/app/core/guard/authGuard.ts
+++ src/app/core/guard/authGuard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { ManterConectadoService } from "../services/manterConectado.service";
 
 @Injectable({ providedIn: "root" })
 
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
     constructor(
         private router: Router,
         private manterConectadoService: ManterConectadoService
